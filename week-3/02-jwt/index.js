5f43ba051719623b69297e6a3a54cbef814b8b4c
Atreay  Kukanur <www.atreayk@gmail.com>
2023-12-23 15:23:38 +0530

    week-3 jwt assignment

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod = require("zod");
 
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 
 /**
  * Generates a JWT for a given username and password.
@@ -14,7 +17,18 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }
+
+    const signature = jwt.sign({
+        username
+    }, jwtPassword);
+
+    return signature;
 }
 
 /**
@@ -26,7 +40,13 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    let ans = true;
+    try{
+        jwt.verify(token, jwtPassword); //jwt.verify() is the most chotic function because it will panic and throw an error if the token is not valid.
+    } catch(e) {
+        ans = false;
+    }
+    return ans;
 }
 
 /**
@@ -37,7 +57,12 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    const decoded = jwt.decode(token); //It will return the decoded data from the string if the string(jwt) is valid otherwise it will return NULL
+    if(decoded){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
